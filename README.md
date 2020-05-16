4a9ba5c21f5b7130d75338a7b0d112cfb92b2f96
virgosoy <virgosoy@126.com>
2020-05-16 19:20:41 +0800

    add convert.js

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sy-util
-js utils
+javascript utils
+
+
+
+## convert.js
+
+类型转换
+
+esmodule类型，使用原生js
+
+### 1.0.0.200516
+
+新增方法：
+
+arrToObj - 数组转对象
+
+objToArr - 对象转数组
+
---
+++ convert.js
@@ -0,0 +1,28 @@
+
+/* 类型转换 */
+
+/**
+ * 数组转对象，浅克隆
+ * var arr = [{key:'name',label:'姓名'},{key:'age',label:'年龄'}]
+ * arrToObj(arr,'key') // {name:{key:'name',label:'姓名'},age:{key:'age',label:'年龄'}}
+ * @param {Array} arr 要转换的数组
+ * @param {String} key 指定数组元素中的什么key的值作为转换后对象的key
+ */
+export function arrToObj(arr,key){
+    let result = {}
+    arr.forEach(item => {
+        result[item[key]] = item
+    })
+    return result
+}
+
+/**
+ * 对象转数组
+ * var obj = {name:{label:'姓名'},age:{label:'年龄'}}
+ * objToArr(obj,'key') // [{key:'name',label:'姓名'},{key:'age',label:'年龄'}]
+ * @param {Object} obj 要转换的对象
+ * @param {String} key 指定对象的key转为数组元素的key的什么名称
+ */
+export function objToArr(obj,key){
+    return Objects.entitys(obj).map(([k,v])=>({[key]:k,...v}))
+}
