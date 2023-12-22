5144ef0a936429893005eccf92dc5d7158bf0fc4
virgosoy <virgosoy@126.com>
2023-12-22 14:23:26 +0800

    feat(nuxt): [useSseClient] 替换EventSource实现为fetchEventSource并支持泛型

--- nuxt/composables/useSseClient.ts
+++ nuxt/composables/useSseClient.ts
@@ -2,46 +2,54 @@
 import type { NitroFetchRequest } from 'nitropack'
 import { safeDestr } from 'destr'
 import { tryOnScopeDispose } from '@vueuse/core'
+import { fetchEventSource } from '@microsoft/fetch-event-source'
 /**
  * Service-Send Events（SSE）客户端组合式工具
  * @since 2023-12-19
- * @version 2023-12-22 为onReceive函数增加泛型支持
+ * @version 2023-12-22 为onReceive函数增加泛型支持，替换EventSource实现为fetchEventSource并支持泛型
  * @changeLog
- *        2023-12-22 为onReceive函数增加泛型支持
+ *        2023-12-22 为onReceive函数增加泛型支持，替换EventSource实现为fetchEventSource并支持泛型
  *        2023-12-19 初始版本
  */
-export function useSseClient(url: NitroFetchRequest){
-  const receiveHandlerList: ((data: unknown) => void)[] = []
-  const eventSource = new EventSource(url as string)
-  eventSource.onmessage = (event) => {
-    console.log(event.data)
-    const data = safeDestr(event.data)
-    receiveHandlerList.map(f => f(data))
-  }
-  eventSource.onerror = (event) => {
-    console.error('EventSource failed:', event);
-    eventSource.close();
-  }
-  eventSource.addEventListener('close', () => {
-    console.log('Server will close the the connection ...')
-    eventSource.close();
+export function useSseClient<Res, Req = any>(url: NitroFetchRequest, opt?: {method?: 'GET' | 'POST' | 'get' | 'post', body?: Req}){
+  const receiveHandlerList: ((data: Res) => void)[] = []
+
+  const ctrl = new AbortController();
+  fetchEventSource(url, {
+    method: opt?.method,
+    body: opt?.body === undefined ? undefined : JSON.stringify(opt.body),
+    signal: ctrl.signal,
+    onmessage(event) {
+      if (event.event === 'close') {
+        console.log(`Close event trigger. Server will close the the connection ...`,  event)
+        ctrl.abort()
+        return
+      }
+      console.log(`event: ${event.event} data: ${event.data}`)
+      const data = safeDestr<Res>(event.data)
+      receiveHandlerList.map(f => f(data))
+    },
+    onerror(error) {
+      // console.error('EventSource failed:', error)
+      throw error;
+    },
   })
   tryOnScopeDispose(() => {
-    eventSource.close();
+    ctrl.abort()
   })
   return {
     /**
      * 接收消息回调
      * @param receiveHandler 
      */
-    onReceive<T>(receiveHandler: (data: T) => void){
+    onReceive(receiveHandler: (data: Res) => void){
       receiveHandlerList.push(receiveHandler)
     },
     /**
      * 客户端关闭 SSE 连接
      */
     close(){
-      eventSource.close();
+      ctrl.abort()
     }
   }
 }
